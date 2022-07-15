fc7227f049bed9faed899ab7d886d682c235f6a6
Tskate <kate353@ukr.net>
2022-07-15 18:42:19 +0300

    feat: Add page templates and routing by them

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from "react";
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import './App.css';
+import Main from "./pages/MainPage/Main";
+import Voting from "./pages/VotingPage/Voting";
+import Breeds from "./pages/BreedsPage/Breeds";
+import Gallery from "./pages/GalleryPage/Gallery";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+      <div className="App">
+          <h1>Hi</h1>
+          <BrowserRouter>
+            <Routes>
+              <Route exact path="/" element={<Main />} />
+              <Route exact path="/voting" element={<Voting />} />
+              <Route exact path="/breeds" element={<Breeds />} />
+              <Route exact path="/gallery" element={<Gallery />}/>
+              <Route path="*" element={<Navigate replace to="/" />}/>
+            </Routes>
+          </BrowserRouter>
+      </div>
   );
 }
 
